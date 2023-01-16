77a7312fa40c8386c965805bb9c58762db66a161
Kanishk Sachdev <kanishksachdev@gmail.com>
2023-01-16 18:27:08 -0500

    changes to the bot and updated functionality for the library

--- bot.py
+++ bot.py
@@ -12,6 +12,7 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 intents=discord.Intents.default()
 intents.message_content = True
 
+
 bot = commands.Bot(command_prefix='$', intents=intents, help_command=None)
 
 
